7cb492381a7638d2e3dbdd91a185cae0a9a63c55
Matheus Alves <matheus@email.com>
2025-06-11 22:03:40 -0300

    Adição de uma tela simples para visualização do projeto

--- src/main/java/com/teste/Locadora/controller/ClienteController.java
+++ src/main/java/com/teste/Locadora/controller/ClienteController.java
@@ -3,10 +3,12 @@ package com.teste.Locadora.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import com.teste.Locadora.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("/cliente")
+@CrossOrigin(origins = "*")
 public class ClienteController {
 
     @Autowired
@@ -39,5 +42,10 @@ public class ClienteController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         repository.deleteById(id);
+    }
+
+     @PutMapping 
+    public Cliente update(@RequestBody Cliente obj) {
+        return repository.save(obj);
     }
 }
